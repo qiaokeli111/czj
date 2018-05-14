440d0dc1680e78b0f51e4c23b30fccd691bdc2a3
527454983@qq.com <527454983@qq.com>
2018-05-14 11:30:01 +0800

    测试

---
+++ src/localrep/test.java
@@ -0,0 +1,18 @@
+
+package localrep;
+
+
+public class test {
+    String aa = "222";
+    public void number(String aa) {
+        this.aa = aa ;
+        System.out.println(aa); 
+    }
+   public static void main(String[] args) {
+    test bb = new test();
+    bb.number(bb.aa);
+}
+}
+
+
+
